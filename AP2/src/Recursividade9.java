e7ba6b510306270fa36c9f315d87b3ff5e603adc
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2022-09-30 08:05:50 -0300

    l

--- AP2/src/Recursividade9.java
+++ AP2/src/Recursividade9.java
@@ -1,3 +1,21 @@
 public class Recursividade9 {
-    
+
+    public static int menor(int v[]) {
+
+        
+
+
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        int vetor[]={1,2,3,4,5,6,7,8,9};
+
+        System.out.println(menor(vetor));
+
+    }
 }
